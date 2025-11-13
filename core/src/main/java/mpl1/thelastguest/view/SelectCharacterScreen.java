6389d62619fb714246587cdd1750c4776d7be5b1
Hantaou Karim <karim.hantaou@gmail.com>
2025-11-13 16:16:16 +0100

    Fix: CharacterSelect ghost button

--- core/src/main/java/mpl1/thelastguest/view/SelectCharacterScreen.java
+++ core/src/main/java/mpl1/thelastguest/view/SelectCharacterScreen.java
@@ -138,7 +138,9 @@ public class SelectCharacterScreen implements Screen {
     @Override public void resize(int width, int height) {}
     @Override public void pause() {}
     @Override public void resume() {}
-    @Override public void hide() {}
+    @Override public void hide() {
+        Gdx.input.setInputProcessor(null);
+    }
 
     @Override
     public void dispose() {
