d0842d85402b8e1467a72bc43b6d1d75f30875f2
Bastient6 <bastien.tiffy@epitech.eu>
2025-11-21 16:47:50 +0100

    [UPDATE]_javadoc

--- core/src/main/java/mpl1/thelastguest/controller/EndController.java
+++ core/src/main/java/mpl1/thelastguest/controller/EndController.java
@@ -2,14 +2,27 @@ package mpl1.thelastguest.controller;
 
 import mpl1.thelastguest.Main;
 import mpl1.thelastguest.view.EndScreen;
-
+/**
+ * Controller for display end game
+ * <p>
+ * This class interact with this view {@link EndScreen} and the principal game {@link Main}
+ * Its objective is to trigger the action leading to the end of the game.
+ */
 public class EndController {
     private final Main game;
 
+    /**
+     * It's the construtor of the class
+     *
+     * @param game instance of the principal game {@link Main}
+     */
     public EndController(Main game, EndScreen view) {
         this.game = game;
     }
 
+    /**
+     * It's for restart game
+     */
     public void play(){
         game.screenManager.showCharacterSelection();
     }
--- core/src/main/java/mpl1/thelastguest/controller/GameController.java
+++ core/src/main/java/mpl1/thelastguest/controller/GameController.java
@@ -13,13 +13,19 @@ import mpl1.thelastguest.model.Character.Character;
 import mpl1.thelastguest.model.Item.ActionItem;
 import mpl1.thelastguest.model.Item.Item;
 import mpl1.thelastguest.model.Room;
+import mpl1.thelastguest.view.EndScreen;
 import mpl1.thelastguest.view.GameScreen;
 import mpl1.thelastguest.view.ui.notification.Notification;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+/**
+ * Controller for all game
+ * <p>
+ * This class interact with all class for the game logic
+ * manage the turn by turn, all interaction of pnj and player
+ */
 public class GameController {
     private final Main game;
     private Player player;
@@ -34,6 +40,16 @@ public class GameController {
     private boolean leftClicked = false;
     private boolean deadRound = false;
 
+    /**
+     * it's the constructor of the class
+     * <p>
+     * @param game instance of the principal game {@link Main}
+     * @param view instance of the display class for game {@link GameScreen}
+     * @param player instance of the class player (it's the current player of the game) {@link Player}
+     * @param npcs list of instance of npc (it's all pnj on the game {@link Npc}
+     * @param murderer instance of the class murderer (it's the current murderer of the game) {@link Murderer}
+     * @param items list of instance of item (it's all items on the game) {@link Item}
+     */
     public GameController(Main game, GameScreen view, Player player, List<Npc> npcs, Murderer murderer, List<Item> items) {
         this.game = game;
         this.view = view;
@@ -51,18 +67,36 @@ public class GameController {
         this.board = new Board(1600 / 50, this.npcs, this.player, this.items, view.getTiledSize());
     }
 
+    /**
+     * get the private list of Npc of the game
+     * @return the list of Character on the game
+     */
     public List<Character> getNpcs() {
         return this.npcs;
     }
 
+    /**
+     * get the private instance of player
+     * @return the isntance of curretn player
+     */
     public Player getPlayer() {
         return this.player;
     }
 
+    /**
+     * get the private instance of board
+     * @return the instance of board
+     */
     public Board getBoard(){
         return this.board;
     }
 
+    /**
+     * Action for the pnj and the murderer
+     * for all turn,
+     * the pnj move to random point and take random item
+     * the murderer move to random point, take random item and kill random people
+     */
     public void startRound() {
         if (currentNpc == this.npcs.size()) {
             return;
@@ -96,6 +130,11 @@ public class GameController {
         }
     }
 
+    /**
+     * count all npcs is alive
+     * @param npcs
+     * @return the number of alive npc
+     */
     public int remainingNpc(List<Character> npcs) {
         int result = 0;
         for (Character npc : npcs) {
@@ -104,6 +143,10 @@ public class GameController {
         return result;
     }
 
+    /**
+     * this methode its call for all loop of game, and manage the turn by turn for pnj or for the player
+     * for player handle all input
+     */
     public void update(float delta) {
         if(!player.isAlive()){
             if(!isSaved()) game.screenManager.showEnd(murderer, player);
@@ -162,6 +205,10 @@ public class GameController {
         }
     }
 
+    /**
+     * verify if one menu are open
+     * @return true if one menu are open false otherwise
+     */
     public boolean isMenuOpen(){
         return
             view.isActionMenuOpen()
@@ -172,11 +219,21 @@ public class GameController {
             || view.isPauseMenuOpen();
     }
 
+    /**
+     * close menu of action
+     * if the inventory of room is closed, the inventory of th player
+     */
     public void closeActionMenu(){
         view.closeActionMenu();
         if (!view.isRoomInventoryOpen()) view.getPlayerInventory().rebuild();
     }
 
+    /**
+     * if player has enough action points
+     * move the player to the pos x y of the window (is divide my the tiled size on map)
+     * @param x posx of screen
+     * @param y posy of screen
+     */
     public void move(float x, float y){
         Vector3 worldPos = view.getCamera().unproject(new Vector3(x, y, 0));
         if(!board.moveToPoint((int) (worldPos.x /view.getTiledSize()), (int) (worldPos.y /view.getTiledSize()))){
@@ -186,6 +243,9 @@ public class GameController {
         }
     }
 
+    /**
+     * if player as enough action points generate new fingerprints for the player
+     */
     public void spoofFingerprints(){
 
         if(!player.enoughAp(1)) return;
@@ -197,7 +257,13 @@ public class GameController {
         closeActionMenu();
     }
 
-
+    /**
+     * if player as enough action points,
+     * scan fingerprints present on the item (weapon) and
+     * if player has good luck, the fingerprints are display
+     * otherwise notiffication display No fingerprints found
+     * @param item item to scan
+     */
     public void scanFingerprints(Item item){
 
         if(!player.enoughAp(1)) return;
@@ -212,6 +278,13 @@ public class GameController {
         view.getNotificationManager().addNotification(notification);
     }
 
+    /**
+     * if player as enough action points,
+     * scan fingerprints present on the Character and
+     * if the player has more strength than the character the fingerprints are display
+     * otherwise notification display won't let you scan him without fighting back
+     * @param ch character to scan
+     */
     public void scanFingerprints(Character ch){
 
         if(!player.enoughAp(1)) return;
@@ -230,6 +303,13 @@ public class GameController {
         view.getNotificationManager().addNotification(notification);
     }
 
+    /**
+     * if player as enough action points,
+     * scan clues fingerprints present on the Character and
+     * if the player has enough luck, display the npc clues fingerprints
+     * otherwise notification display No fingerprints found
+     * @param npc character to scan
+     */
     public void scanClueFingerprints(Character npc){
 
         if(!player.enoughAp(2)) return;
@@ -245,6 +325,13 @@ public class GameController {
         view.getNotificationManager().addNotification(notification);
     }
 
+    /**
+     * if player as enough action points,
+     * inspect the character (victim)
+     * if the player has enough perception, display the Wound type
+     * otherwise notification display No wounds found...
+     * @param npc character to scan
+     */
     public void inspect(Character npc){
 
         if(!player.enoughAp(1)) return;
@@ -257,6 +344,13 @@ public class GameController {
     }
     // ROOM SEARCH
 
+    /**
+     * if player as enough action points,
+     * search item in room
+     * if the room is locked, display The room is locked
+     * otherwise display item present in room only first nbrItems of the list
+     * @param nbrItems number of display item
+     */
     public void search(int nbrItems){
         if(!enoughAp(1)) return;
 
@@ -274,6 +368,11 @@ public class GameController {
         }
     }
 
+    /**
+     * if player as enough action points,
+     * if the player has a key item,
+     * the room are unlocked
+     */
     public void unlock(){
 
         if(!player.enoughAp(1)) return;
@@ -283,6 +382,11 @@ public class GameController {
         view.getNotificationManager().addNotification(new Notification("You unlocked the room"));
     }
 
+    /**
+     * add item on inventory
+     * if player don't have enough space, a notification displayed Not enough space in the inventory.
+     * @param item it's item to pick
+     */
     public void pickItem(Item item){
         if(player.pickItem(item)){
             Room actualRoom = board.findRoom(player.getRoom());
@@ -295,6 +399,10 @@ public class GameController {
         }
     }
 
+    /**
+     * close room inventory
+     * and rebuild the player inventory
+     */
     public void closeRoomInventory(){
         view.closeRoomInventory();
         view.getPlayerInventory().rebuild();
@@ -302,16 +410,27 @@ public class GameController {
 
     // PLAYER INVENTORY
 
+    /**
+     * display menu of action for the item
+     * @param item Action item
+     */
     public void displayItemActionMenu(Item item){
         Vector2 mousePosition = new Vector2(Gdx.input.getX(), Gdx.input.getY());
         view.displayItemActionMenu(mousePosition, item);
     }
 
+    /**
+     * close action menu of item
+     */
     public void closeItemActionMenu(){
         view.closeItemActionMenu();
         view.getPlayerInventory().rebuild();
     }
 
+    /**
+     * drop select item
+     * @param item item to drop
+     */
     public void dropItem(Item item){
         if(player.dropItem(item)){
             Room actualRoom = board.findRoom(player.getRoom());
@@ -320,27 +439,47 @@ public class GameController {
         }
     }
 
+    /**
+     * destroys the item when it can no longer be used
+     * @param item item to destroy
+     */
     public void destroyItem(Item item){
         player.dropItem(item);
         view.getNotificationManager().addNotification(new Notification(item.getName() + " out of use"));
         view.getPlayerInventory().rebuild();
     }
 
+    /**
+     * display description of item
+     * @param item item to display description
+     */
     public void displayDescription(Item item){
         view.getNotificationManager().addNotification(new Notification(item.getDescription(), 5f));
     }
 
     // GUESS MENU
 
+    /**
+     * display menu guess for try to find the murderer
+     */
     public void displayGuessMenu(){
         view.displayGuessMenu();
     }
 
+    /**
+     * close menu guess
+     */
     public void closeGuessMenu(){
         view.closeGuessMenu();
         view.getPlayerInventory().rebuild();
     }
 
+    /**
+     * to handle the result of guess menu
+     * if the selected characetr it's the murderer display You found the murderer and show end
+     * otherwise display character was an innocent and kill this character
+     * @param character selected character on guess menu
+     */
    public void guess(Character character){
         view.closeGuessMenu();
         view.getPlayerInventory().rebuild();
@@ -364,10 +503,17 @@ public class GameController {
     }
 
     // PLAYER MENU
+
+    /**
+     * display menu player
+     */
     public void displayPlayerMenu(){
         view.displayPlayerMenu();
     }
 
+    /**
+     * close menu player and rebuild inventory
+     */
     public void closePlayerMenu(){
         view.closePlayerMenu();
         view.getPlayerInventory().rebuild();
@@ -375,19 +521,31 @@ public class GameController {
 
     // PAUSE MENU
 
+    /**
+     * display pause menu
+     */
     public void displayPauseMenu(){
         view.displayPauseMenu();
     }
 
+    /**
+     * close pause menu and rebuild inventory
+     */
     public void closePauseMenu(){
         view.closePauseMenu();
         view.getPlayerInventory().rebuild();
     }
 
+    /**
+     * restart game
+     */
     public void restartGame(){
         game.screenManager.showCharacterSelection();
     }
 
+    /**
+     * active godmode and set all stats to 999
+     */
     public void godMode(){
         player.setStr(999);
         player.setStartAp(999);
@@ -397,12 +555,21 @@ public class GameController {
         player.setInv(999);
     }
 
+    /**
+     * exit game
+     */
     public void exitGame(){
         Gdx.app.exit();
     }
 
     // PLAYER AP
 
+    /**
+     * return true if the player have enough action points and update action points
+     * return false otherwise and display Not enough action points
+     * @param ap action points
+     * @return boolean
+     */
     public boolean enoughAp(int ap){
         if(player.enoughAp(ap)){
             player.setAp(player.getAp() - ap);
@@ -413,6 +580,11 @@ public class GameController {
         }
     }
 
+    /**
+     * if the player has item with action save the player is saved, the item is destroyed, display You have been saved and return true
+     * otherwise return  false
+     * @return boolean
+     */
     public boolean isSaved(){
         if(player.canDoAction("save")) {
             player.setAlive(true);
@@ -427,10 +599,19 @@ public class GameController {
 
     // SKULL ACTION
 
+    /**
+     * verify if the player can do action skull
+     * @return boolean
+     */
     public boolean isSkull(){
         return player.canDoAction("skull");
     }
 
+    /**
+     * if player has enough perception, the skull is activated and return true
+     * @param per minimum perception
+     * @return boolean
+     */
     public boolean skullStats(int per){
         if(player.getPer() >= per){
             return true;
@@ -440,10 +621,18 @@ public class GameController {
         }
     }
 
+    /**
+     * if player has enough perception
+     * add notification if a new merder have been made
+     */
     public void skullDead(){
         if(isSkull() && skullStats(5)) view.getNotificationManager().addNotification(new Notification("The skull vibrated... someone's perished"));
     }
 
+    /**
+     * if player has enough perception
+     * add notification if a murderer is near
+     */
     public void skullCloseMurderer(){
         if(
             isSkull()
@@ -457,6 +646,10 @@ public class GameController {
         }
     }
 
+    /**
+     * return view
+     * @return View
+     */
     public GameScreen getView() {
         return view;
     }
