949cb8433af8e23b8050d30088c0bd00612a6054
Bastient6 <bastien.tiffy@epitech.eu>
2025-11-23 15:59:41 +0100

    [UPDATE]_items javadoc

--- core/src/main/java/mpl1/thelastguest/model/Item/ActionItem.java
+++ core/src/main/java/mpl1/thelastguest/model/Item/ActionItem.java
@@ -2,17 +2,42 @@ package mpl1.thelastguest.model.Item;
 
 import java.util.Map;
 
+/**
+ * Represents an item capable of performing a specific action.
+ * <p>
+ * Unlike {@link StatItem}, an ActionItem does not modify character statistics.
+ * Instead, it grants the ability to perform special actions such as "kill" or "open door".
+ */
 public class ActionItem extends Item {
 
     private String action;
 
-    public ActionItem(){super();}
+    /**
+     * Default constructor .
+     */
+    public ActionItem(){
+        super();
+    }
 
+    /**
+     * Creates an ActionItem that grants a specific action.
+     *
+     * @param name the name of the item
+     * @param action the action that this item enables
+     */
     public ActionItem(String name, String action) {
         super(name);
         this.action = action;
     }
 
+    /**
+     * Creates an ActionItem with an associated action and a wound type,
+     * used for items that can kimm.
+     *
+     * @param name the name of the item
+     * @param action the action that this item enables
+     * @param woundType the wound type caused when the item is used as a weapon
+     */
     public ActionItem(String name, String action, String woundType) {
         super(name);
         this.action = action;
@@ -21,14 +46,30 @@ public class ActionItem extends Item {
 
     // GETTER
 
+    /**
+     * Returns the action associated with this item.
+     *
+     * @return the action name
+     */
     @Override
     public String getAction() {
         return this.action;
     }
 
+    /**
+     * Action items do not provide statistic , so this always returns null.
+     *
+     * @return null, since ActionItem has no stats
+     */
+
     @Override
     public Map<String, Integer> getStats(){return null;}
 
+    /**
+     * Returns the wound type caused by this item, if any.
+     *
+     * @return the wound type, or null if the item does not inflict wounds
+     */
     @Override
     public String getWoundType() {
         return woundType;
--- core/src/main/java/mpl1/thelastguest/model/Item/Item.java
+++ core/src/main/java/mpl1/thelastguest/model/Item/Item.java
@@ -2,6 +2,11 @@ package mpl1.thelastguest.model.Item;
 
 import java.util.Map;
 
+/**
+ * Base abstract class for all items in the game.
+ * <p>
+ * An {@link Item} may represent objects such as weapons, clues, or stat modifiers.
+ */
 public abstract class Item {
 
     private String name;
@@ -10,20 +15,40 @@ public abstract class Item {
     private String description;
     protected String woundType = null;
 
+    /**
+     * Returns whether the fingerprint of this item has been discovered.
+     * true if the fingerprint is found, false otherwise
+     */
     public boolean isFingerPrintFound() {
         return fingerPrintFound;
     }
+
+    /**
+     * Sets whether the player has discovered this item's fingerprint.
+     *
+     * @param fingerPrintFound true if the fingerprint is found
+     */
 
     public void setFingerPrintFound(boolean fingerPrintFound) {
         this.fingerPrintFound = fingerPrintFound;
     }
 
+
+    /**
+     * Creator with the given name.
+     * A random fingerprint is automatically assigned
+     * from the set {"A", "L", "W"}.
+     * @param name the name of the item
+     */
     public Item(String name){
         this.name = name;
         String[] fingerprints = {"A", "L", "W"};
         this.fingerprint =  fingerprints[(int)(Math.random() * fingerprints.length)];
     }
 
+    /**
+     * default constructor
+     */
     public Item(){
         String[] fingerprints = {"A", "L", "W"};
         this.fingerprint =  fingerprints[(int)(Math.random() * fingerprints.length)];
@@ -31,18 +56,55 @@ public abstract class Item {
 
     // GETTER
 
+    /**
+     * Returns the name of the item.
+     *
+     * @return the item's name
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     * Returns the fingerprint of the item.
+     *
+     * @return the fingerprint's name
+     */
     public String getFingerprint(){return this.fingerprint;}
+
+    /**
+     * Assigns a fingerprint identifier to this item.
+     *
+     * @param fingerprint the new fingerprint value
+     */
     public void setFingerprint(String fingerprint){this.fingerprint = fingerprint;}
 
+    /**
+     * Returns the woundType of the item.
+     *
+     * @return the woundType's name
+     */
     public String getWoundType(){return this.woundType;}
 
+    /**
+     * Returns the statistical effects of this item.
+     * This is implemented by subclasses such as {@link StatItem}.
+     * @return a map of stat modifiers, or null if none
+     */
     abstract Map<String, Integer> getStats();
+
+    /**
+     * Returns the action granted by this item, if any.
+     * Implemented by subclasses such as {@link ActionItem}.
+     * @return the action name, or null if the item provides no action
+     */
     abstract String getAction();
 
+    /**
+     * Returns the description of the item.
+     *
+     * @return the description's name
+     */
     public String getDescription() {
         return description;
     }
