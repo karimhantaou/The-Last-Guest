97ffa4866204ef976359b9aa507a9e05f552decd
Bastient6 <bastien.tiffy@epitech.eu>
2025-11-23 16:11:18 +0100

    [UPDATE]_Main javadoc

--- core/src/main/java/mpl1/thelastguest/Main.java
+++ core/src/main/java/mpl1/thelastguest/Main.java
@@ -7,10 +7,21 @@ import mpl1.thelastguest.view.MenuScreen;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+/**
+ * The main entry point of the game.
+ * <p>
+ * This class initializes the game, manages the main SpriteBatch,
+ * and sets up the initial screen (MenuScreen).
+ */
 public class Main extends Game {
     public SpriteBatch batch;
     public ScreenManager screenManager;
 
+    /**
+     * Called when the game is first created.
+     * Initializes the SpriteBatch and ScreenManager,
+     * and sets the initial screen to the menu.
+     */
     @Override
     public void create() {
         batch = new SpriteBatch();
@@ -18,11 +29,19 @@ public class Main extends Game {
         setScreen(new MenuScreen(this));
     }
 
+    /**
+     * Called continuously to render the game.
+     * Delegates rendering to the currently active screen.
+     */
     @Override
     public void render() {
         super.render();
     }
 
+    /**
+     * Called when the game is closed.
+     * Disposes of the SpriteBatch and the active screen.
+     */
     @Override
     public void dispose() {
         batch.dispose();
